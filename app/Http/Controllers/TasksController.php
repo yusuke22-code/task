370651d46d1641783623f2699ded0441acb5a331
s.yusuke <mrs.aoringo@gmail.com>
2019-08-19 10:30:06 +0000

    kadai-tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Task;
+    
 class TasksController extends Controller
 {
     /**
@@ -13,7 +15,11 @@ class TasksController extends Controller
      */
     public function index()
     {
-        //
+         $tasks = Task::all();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
     }
 
     /**
@@ -23,7 +29,11 @@ class TasksController extends Controller
      */
     public function create()
     {
-        //
+        $task = new Task;
+        
+        return view('tasks.create',[
+            'task' => $task,
+            ]);
     }
 
     /**
@@ -34,7 +44,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
     }
 
     /**
@@ -45,7 +59,11 @@ class TasksController extends Controller
      */
     public function show($id)
     {
-        //
+        $task = Task::find($id);
+        
+        return view('tasks.show',[
+            'task' => $task,
+            ]);
     }
 
     /**
@@ -56,7 +74,11 @@ class TasksController extends Controller
      */
     public function edit($id)
     {
-        //
+        $task = Task::find($id);
+        
+        return view('tasks.edit',[
+            'task' => $task,
+            ]);
     }
 
     /**
@@ -68,7 +90,11 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $task = Task::find($id);
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
     }
 
     /**
@@ -79,6 +105,9 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $task = Task::find($id);
+        $task->delete();
+        
+        return redirect('/');
     }
 }
